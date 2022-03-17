69c3cde22992c62b1a06264741acf7ca358b01d7
Robson <relative@gmail.com>
2022-03-17 15:30:56 +0000

    Update the regular expressions to match IMDb's new HTML

--- GetImdbFilmFranchiseData/Program.cs
+++ GetImdbFilmFranchiseData/Program.cs
@@ -13,6 +13,7 @@ namespace GetImdbFilmFranchiseData
     using System;
     using System.Collections.Generic;    
     using System.IO;
+    using System.Linq;
     using System.Net;
     using System.Text;
     using System.Text.RegularExpressions;
@@ -42,10 +43,11 @@ namespace GetImdbFilmFranchiseData
             var series = new Series
             {
                 Name = name,
+                Wikipedia = ids.First(),
                 Films = new List<Series.Film>()
             };
 
-            foreach (var id in ids)
+            foreach (var id in ids.Skip(1))
             {
                 var bytes = Client.DownloadData("https://www.imdb.com/title/" + id);
                 var html = Encoding.UTF8.GetString(bytes).Replace("\n", string.Empty);
@@ -64,17 +66,18 @@ namespace GetImdbFilmFranchiseData
                 Id = id,
                 Year = int.Parse(Regex.Match(html, "(\\d{4})\\) - IMDb</title>").Groups[1].Value),
                 Name = Regex.Match(html, "<h1[^>]*>([^<]+)").Groups[1].Value.Replace("&nbsp;", " ").Trim(),
-                Rating = decimal.Parse(Regex.Match(html, "<span itemprop=\"ratingValue\">([^<]+)</span>").Groups[1].Value),
-                Votes = int.Parse(Regex.Match(html, "itemprop=\"ratingCount\">([^<]+)</span>").Groups[1].Value.Replace(",", string.Empty)),
-                ReleaseDate = Regex.Match(html, "title=\"See more release dates\" *>([^<]+)<").Groups[1].Value.Trim(),
-                Metascore = Regex.Match(html, "<div class=\"metacriticScore[^>]*>[^<]*<span>([^<]+)</span>").Groups[1].Value,
-                Poster = Regex.Match(html, "<div class=\"poster\">[^>]*<a[^>]+>[^<]*<img[^>]+src=\"([^\"]+)\"").Groups[1].Value,
+                Rating = decimal.Parse(Regex.Match(html, "aggregateRating.*,\"ratingValue\":([\\d\\.]+)\\}").Groups[1].Value),
+                Votes = int.Parse(Regex.Match(html, ",\"ratingCount\":(\\d+),").Groups[1].Value.Replace(",", string.Empty)),
+                Metascore = Regex.Match(html, "{\"metascore\":{\"score\":([\\d+]+),\"").Groups[1].Value,
+                Poster = Regex.Match(html, "<img alt=\"[^\"]+\" class=\"ipc-image\" loading=\"lazy\" src=\"([^\"]+)\"").Groups[1].Value,
             };
         }
 
         private class Series
         {
             public string Name { get; set; }
+
+            public string Wikipedia { get; set; }
 
             public List<Film> Films { get; set; }
 
@@ -89,8 +92,6 @@ namespace GetImdbFilmFranchiseData
                 public decimal Rating { get; set; }
 
                 public int Votes { get; set; }
-
-                public string ReleaseDate { get; set; }
 
                 public string Metascore { get; set; }
 
